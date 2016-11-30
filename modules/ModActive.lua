ae1e84fb9ea96b04bf856b8878376606a823dc07
Murfalo <murphycangelo@u.northwestern.edu>
2016-11-30 11:47:19 -0600

    Implement SFX and music

--- modules/ModActive.lua
+++ modules/ModActive.lua
@@ -1,4 +1,5 @@
 local ModActive = Class.create("ModActive", Entity)
+local hitSound = love.audio.newSource("/assets/sounds/GenericImpact.wav")
 
 ModActive.trackFunctions = {"setHitState","normalState","hitState","onDeath","onHitConfirm","onKill"}
 
@@ -128,6 +129,7 @@ function ModActive:setHitState(stunTime, forceX, forceY, damage, element,faction
 		
 		self.stun = st
 
+		hitSound:play()
 		self:setHealth(self.redHealth - dm)
 		if not self.superArmor then
 			self.body:setLinearVelocity((forceX or 0) * ratio, (forceY or 0) * ratio)
