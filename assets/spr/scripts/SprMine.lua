d635f6c23ea7c44ad5465c25d9a771ecf791cd7e
LuoLei_Zhao <LuoLei_Zhao>
2016-11-30 01:29:34 -0600

    Stay on the ground you stupid mines!:

--- assets/spr/scripts/SprMine.lua
+++ assets/spr/scripts/SprMine.lua
@@ -5,8 +5,8 @@ local pce = {
 	path = "assets/spr/mine.png",
 	width = 32,
 	height = 32,
-	imgX = 32,
-	imgY = 32,
+	imgX = 16,
+	imgY = 16,
 	attachPoints = 	{ center = {x = 16,y = 16}},
 	animations = ane
 }
